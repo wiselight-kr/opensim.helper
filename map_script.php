98b9f7e7e0862d7a902a6b4493e4a7db527bcca1
iseki <iseki@1f05a763-db90-4aae-a935-c908e4079ec2>
2010-05-18 06:57:39 +0000

    first

---
+++ map_script.php
@@ -0,0 +1,111 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////////
+//
+// Modified From OpenSim WebInterface Redux v0.28 
+//													by Fumi.Iseki
+//
+//
+// $size, $centerX, $centerY, $world_map_url, CMS_MODULE_PATH are needed
+//
+
+//
+require_once(CMS_MODULE_PATH."/include/opensim.func.php");
+
+
+
+$display_marker = "dr";	// infomation marker
+
+
+if ($size==16){
+	$minuszoom = 0;   $pluszoom = 32;  $infosize = 8;
+}
+else if ($size==32){
+	$minuszoom = 16;  $pluszoom = 64;  $infosize = 10;
+}
+else if ($size==64){
+	$minuszoom = 32;  $pluszoom = 128; $infosize = 12;
+}
+else if ($size==128) {
+	$minuszoom = 64;  $pluszoom = 256; $infosize = 20;
+}
+else if ($size==256) {
+	$minuszoom = 128; $pluszoom = 512; $infosize = 40;
+}
+else if ($size==512) {
+	$minuszoom = 256; $pluszoom = 0;   $infosize = 60;
+}
+
+?>
+
+function loadmap() {
+	mapInstance = new ZoomSize(<?php print $size?>);
+	mapInstance = new WORLDMap(document.getElementById('map-container'), {hasZoomControls: false, hasPanningControls: true});
+	mapInstance.centerAndZoomAtWORLDCoord(new XYPoint(<?php print $centerX?>, <?php print $centerY?>), 1);
+<?php
+	$DbLink = new DB;
+	$DbLink->query("SELECT uuid,regionName,serverIP,serverURI,locX,locY,serverHttpPort FROM regions ORDER BY locX");
+
+	while($DbLink->Errno==0 and list($uuid, $regionName, $serverIP, $serverURI, $locX, $locY, $serverHttpPort)=$DbLink->next_record())
+	{
+		$name = opensim_get_region_owner($uuid);
+		$firstN = $name['firstname'];
+		$lastN  = $name['lastname'];
+
+		$dx = 0.00; $dy = 0.00;
+		if ($display_marker=="tl") {
+			$dx = -0.40; 	$dy = 0.40;
+		}
+		else if ($display_marker=="tr") {
+			$dx = 0.40; 	$dy = 0.40;
+		}
+		else if ($display_marker=="dl") {
+			$dx = -0.40; 	$dy = -0.40;
+		}
+		else if ($display_marker=="dr") {
+			$dx = 0.40; 	$dy = -0.40;
+		}
+
+		$locX = $locX/256;
+		$locY = $locY/256;
+		$MarkerCoordX = $locX + $dx;
+		$MarkerCoordY = $locY + $dy;
+
+		$server = "";
+		if ($serverURI!="") {
+    		$dec = explode(":", $serverURI);
+    		if (!strncasecmp($dec[0], "http", 4)) $server = "$dec[0]:$dec[1]";
+		}   
+		if ($server=="") {
+    		$server ="http://$serverIP";
+		}
+		$server = "$server:$serverHttpPort";
+
+		$uuid = str_replace("-", "", $uuid);
+	  	$imageURL = $server."/index.php?method=regionImage".$uuid;
+		$windowTitle = "Region Name: ".$regionName."<br /><br />Coordinates: ".$locX.",".$locY."<br /><br />Owner: ".$firstN." ".$lastN;
+?>
+	  	var tmp_region_image = new Img("<?php print $imageURL?>", <?php print $size?>, <?php print $size?>);
+		var region_loc = new Icon(tmp_region_image);
+		var all_images = [region_loc, region_loc, region_loc, region_loc, region_loc, region_loc];
+		var marker = new Marker(all_images, new XYPoint(<?php print $locX?>, <?php print $locY?>));
+		mapInstance.addMarker(marker);
+	
+		var map_marker_img = new Img("images/info.gif", <?php print $infosize?>, <?php print $infosize?>);
+		var map_marker_icon = new Icon(map_marker_img);
+		var mapWindow = new MapWindow("<?php print $windowTitle?>", {closeOnMove: true});
+		var all_images = [map_marker_icon, map_marker_icon, map_marker_icon, map_marker_icon, map_marker_icon, map_marker_icon];
+		var marker = new Marker(all_images, new XYPoint(<?php print $MarkerCoordX?>, <?php print $MarkerCoordY?>));
+		mapInstance.addMarker(marker, mapWindow);
+<?php
+	}
+	$DbLink->close();
+?>
+}
+
+
+function setZoom(size) {
+	var cord = mapInstance.getMapCenter();
+	window.location.href = "<?php print $world_map_url?>?size="+size+"&ctX="+cord.x+"&ctY="+cord.y+"";
+}
+
+
---
+++ mysql.php
@@ -0,0 +1,199 @@
+<?php
+/*
+ * Copyright (c) 2007, 2008 Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS for a full list of copyright holders.
+ *
+ * See LICENSE for the full licensing terms of this file.
+ *
+*/
+
+//This looks like its lifted from http://www.weberdev.com/get_example-4372.html  
+//I'd contact the original developer for licensing info, but his website is broken.
+
+//
+// Modified by Fumi.Iseki for CMS/LMS '10 3/29
+//
+
+
+// for TEST
+if (!defined("OPENSIM_DB_HOST")) {
+	exit();
+	//define("OPENSIM_DB_HOST",  "202.26.159.xxx");
+	//define("OPENSIM_DB_NAME",  "opensim_xxxx");
+	//define("OPENSIM_DB_USER",  "opensim_xxxx");
+	//define("OPENSIM_DB_PASS",  "opensim_xxxx");
+}
+
+
+
+class DB
+{
+	var $Host 	  = OPENSIM_DB_HOST;	// Hostname of our MySQL server
+	var $Database = OPENSIM_DB_NAME;	// Logical database name on that server
+	var $User 	  = OPENSIM_DB_USER;	// Database user
+	var $Password = OPENSIM_DB_PASS;	// Database user's password
+	var $Link_ID  = 0;					// Result of mysql_connect()
+	var $Query_ID = 0;					// Result of most recent mysql_query()
+	var $Record	  = array();			// Current mysql_fetch_array()-result
+	var $Row;							// Current row number
+	var $Errno    = 0;					// Error state of query
+	var $Error    = "";
+
+
+	/*
+	function DB($connect=false)
+	{
+		$this->Host 	= OPENSIM_DB_HOST;	
+		$this->Database = OPENSIM_DB_NAME;	
+		$this->User 	= OPENSIM_DB_USER;	
+		$this->Password = OPENSIM_DB_PASS;	
+
+		if ($connect) $this->connect();
+	}
+	*/
+
+
+
+	function halt($msg)
+	{
+		echo("</td></tr></table><b>Database error:</b> $msg<br />\n");
+		echo("<b>MySQL error</b>: $this->Errno ($this->Error)<br />\n");
+		die("Session halted.");
+	}
+
+
+
+	// Added by Fumi.Iseki
+	function set_DB($dbhost, $dbname, $dbuser, $dbpass)
+	{
+		$this->Host 	= $dbhost;	
+		$this->Database = $dbname;	
+		$this->User 	= $dbuser;	
+		$this->Password = $dbpass;	
+	}
+
+
+
+	function connect()
+	{
+		if($this->Link_ID==0) {
+			$this->Link_ID = mysql_connect($this->Host, $this->User, $this->Password);
+			if (!$this->Link_ID) {
+				//$this->halt("Link_ID == false, connect failed");
+				$this->Errno = 999;
+				return;
+			}
+			//if (_CHARSET=="UTF-8") mysql_set_charset('utf8'); 
+			mysql_set_charset('utf8'); 
+			$SelectResult = mysql_select_db($this->Database, $this->Link_ID);
+			if (!$SelectResult) {
+				$this->Errno = mysql_errno($this->Link_ID);
+				$this->Error = mysql_error($this->Link_ID);
+				$this->Link_ID = 0;
+				$this->halt("cannot select database <i>".$this->Database."</i>");
+			}
+		}
+	}
+
+
+
+ 	function escape($String)
+ 	{
+ 		return mysql_escape_string($String);
+ 	}
+
+
+
+	function query($Query_String)
+	{
+		$this->connect();
+		if ($this->Errno!=0) return 0; 	// added by Fumi.Iseki
+
+		$this->Query_ID = mysql_query($Query_String,$this->Link_ID);
+		$this->Row = 0;
+		$this->Errno = mysql_errno();
+		$this->Error = mysql_error();
+		if (!$this->Query_ID) {
+			$this->halt("Invalid SQL: ".$Query_String);
+		}
+		return $this->Query_ID;
+	}
+
+
+
+	function next_record()
+	{
+		$this->Record = @mysql_fetch_array($this->Query_ID);
+		$this->Row += 1;
+		$this->Errno = mysql_errno();
+		$this->Error = mysql_error();
+		$stat = is_array($this->Record);
+		if (!$stat) {
+			@mysql_free_result($this->Query_ID);
+			$this->Query_ID = 0;
+		}
+		return $this->Record;
+	}
+
+
+
+	function num_rows()
+	{
+		return mysql_num_rows($this->Query_ID);
+	}
+
+
+
+	function affected_rows()
+	{
+		return mysql_affected_rows($this->Link_ID);
+	}
+
+
+
+	function optimize($tbl_name)
+	{
+		$this->connect();
+		if ($this->Errno!=0) return;
+
+		$this->Query_ID = @mysql_query("OPTIMIZE TABLE $tbl_name",$this->Link_ID);
+	}
+
+
+
+	function clean_results()
+	{
+		if ($this->Query_ID!=0) {
+			mysql_freeresult($this->Query_ID);
+			$this->Query_ID = 0;
+		}
+	}
+
+
+
+	function close()
+	{
+		if ($this->Link_ID) {
+			mysql_close($this->Link_ID);
+			$this->Link_ID = 0;
+		}
+	}
+
+
+
+	// added by Fumi.Iseki
+	function exist_table($table)
+	{
+		$this->connect();
+		if ($this->Errno!=0) return false;
+
+		$tl = mysql_list_tables($this->Database, $this->Link_ID);
+		while($row=mysql_fetch_row($tl)) {
+			if (in_array($table, $row)) return true;
+		}
+		return false;
+	}
+
+}
+
+?>
